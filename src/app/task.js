92bc671a069156b1e44b66b4a0d0241b2b3cb1cc
Justin Tullgren <justin.tullgren@gmail.com>
2016-09-04 19:38:44 -0400

    Move through flows

--- src/app/task.js
+++ src/app/task.js
@@ -2,12 +2,19 @@
 
 const template = require('./task.html');
 const restrict = 'E';
-const link = (scope, elem, attrs, controller) => {
+const getLink = (constants) => ((scope, elem, attrs, controller) => {
+  scope.start = (task) => {
+    task.status = constants.STATUSES.IN_PROGRESS;
+  };
 
-};
+  scope.complete = (task) => {
+    task.status = constants.STATUSES.COMPLETED;
+  };
 
-export const task = () =>({
+});
+
+export const task = (constants) =>({
   template,
   restrict,
-  link
+  link: getLink(constants)
 });
