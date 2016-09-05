13d6d7aeecaa0012cdce36047bfeb02eadae8258
Justin Tullgren <justin.tullgren@gmail.com>
2016-09-05 13:29:24 -0400

    scope soup and filters

--- src/app/board.js
+++ src/app/board.js
@@ -3,11 +3,33 @@
 const template = require('./board.html');
 const restrict = 'E';
 
-export const board = (constants, tasksRepository) => ({
+export const board = ($timeout, constants, tasksRepository) => ({
   template,
   restrict,
   link: scope => {
-    scope.statuses = constants.STATUSES;
-    scope.tasks = tasksRepository.tasks;
+    scope.filterReady = false;
+    scope.filterInProgress = false;
+    scope.filterCompleted = false;
+
+    scope.getTasks = () => {
+      $timeout(() => {
+        scope.tasks = tasksRepository.tasks;
+      }, 500);
+    };
+
+    const init = () => {
+      scope.statuses = constants.STATUSES;
+    };
+
+    const applyFilters = () => {
+      scope.$emit('filter', {
+        ready: scope.filterReady,
+        inProgress: scope.filterInProgress,
+        completed: scope.filterCompleted
+      });
+    };
+    scope.$watchCollection('tasks', applyFilters);
+
+    init();
   }
 });
--- src/app/swimLane.js
+++ src/app/swimLane.js
@@ -2,7 +2,14 @@
 const restrict = 'AE';
 const template = require('./swimLane.html');
 
-export default () => ({
+export default constants => ({
   restrict,
-  template
+  template,
+  link: scope => {
+    scope.filterTask = task => {
+      return (task.status === constants.STATUSES.COMPLETED && scope.filterCompleted) ||
+        (task.status === constants.STATUSES.IN_PROGRESS && scope.filterInProgress) ||
+        (task.status === constants.STATUSES.READY && scope.filterReady);
+    };
+  }
 });
